94eb9090e6abcb0def3a6184c846c7a6cbbdde70
Igor G <igor.gulyaev@gmail.com>
2012-09-29 02:06:47 +0300

    vote functionality added

--- polls/views.py
+++ polls/views.py
@@ -1,29 +1,33 @@
-from django.http import Http404
 from django.shortcuts import render_to_response, get_object_or_404
-from django.template import Context, loader
-from polls.models import Poll
-from django.http import HttpResponse
+from django.template import RequestContext
+from polls.models import Poll, Choice
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 def index(request):
 	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-	#hardoced view commented out
-	#output = ', '.join([p.question for p in latest_poll_list])
-	#here goes new 'zen' view using templates
-	template = loader.get_template('polls/index.html')
-	context = Context({
-		'latest_poll_list':latest_poll_list,
-	})
-	#return HttpResponse(template.render(context))
-	#and even simplier below:
 	return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 	
 def detail(request, poll_id):
 	p = get_object_or_404(Poll, pk=poll_id)
-	return render_to_response('polls/detail.html', {'poll': p})
+	return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
 
 def results(request, poll_id):
-	return HttpResponse("You're looking at the results of poll %s." % poll_id)
+	p = get_object_or_404(Poll, pk=poll_id)
+	return render_to_response('polls/results.html', {'poll': p})
 	
 def vote(request, poll_id):
-	return HttpResponse("You're voting on poll %s." % poll_id)
+	p = get_object_or_404(Poll, pk=poll_id)
+	try:
+		selected_choice = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		#Redisplay poll voting
+		return render_to_response('polls/detail.html', {
+			'poll': p,
+			'error_message': "You didn't select a choice.",
+		}, context_instance=RequestContext(request))
+	else:
+		selected_choice.votes += 1
+		selected_choice.save()	
+	return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
 	
