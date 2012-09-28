43f0c962d527ccfb4b1cebe4f8a3dca04a204553
Igor G <igor.gulyaev@gmail.com>
2012-09-29 01:18:06 +0300

    sample view decorated with template

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,21 @@
+from django.shortcuts import render_to_response
+from django.template import Context, loader
+from polls.models import Poll
 from django.http import HttpResponse
 
 def index(request):
-	return HttpResponse("Hello, World! You are at the poll index")
-
+	latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+	#hardoced view commented out
+	#output = ', '.join([p.question for p in latest_poll_list])
+	#here goes new 'zen' view using templates
+	template = loader.get_template('polls/index.html')
+	context = Context({
+		'latest_poll_list':latest_poll_list,
+	})
+	#return HttpResponse(template.render(context))
+	#and even simplier below:
+	return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
+	
 def detail(request, poll_id):
 	return HttpResponse("You're looking at poll {}.".format(poll_id))
 
