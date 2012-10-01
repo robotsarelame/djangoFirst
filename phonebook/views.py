7c98cb7d84e0e86dc3876ce06ff5706085616604
Igor G <igor.gulyaev@gmail.com>
2012-10-01 22:05:46 +0300

    'Add' fixed and working now

--- phonebook/views.py
+++ phonebook/views.py
@@ -3,6 +3,7 @@ from phonebook.models import Person
 from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.core.urlresolvers import reverse
 
 def list(request):
     contact_list = Person.objects.all().order_by('name')
@@ -23,15 +24,15 @@ def detail(request, person_id):
 	return render_to_response('phonebook/detail.html', {'person': p}, context_instance=RequestContext(request))
 	
 def new(request):
-	return render_to_response('phonebook/contact_add_edit_form.html', {'action': 'add', 'button': 'Add'})
+	return render_to_response('phonebook/contact_add_edit_form.html', {'action': 'add', 'button': 'Add'}, context_instance=RequestContext(request))
 	
 def add(request):
-	person_name = request.POST("person_name")
-	person_phone = request.POST("person_phone")
-	person_email = request.POST("person_email")
+	person_name = request.POST.get("person_name")
+	person_phone = request.POST.get("person_phone")
+	person_email = request.POST.get("person_email")
 	p = Person(name = person_name, phone = person_phone, email = person_email)
 	p.save()
-	return list(request)
+	return HttpResponseRedirect(reverse('phonebook.views.list'))
 
 def edit(request, person_id):
 	p = get_object_or_404(Person, pk=person_id)
